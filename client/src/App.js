008033468e68d74ac90c1a641cf4aafdea50a949
Shubhamsharma1522 <shubhamsharma.in1522@gmail.com>
2024-01-26 23:40:07 +0530

    all pages are done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage.js";
+import About from "./pages/About.js";
+import Contact from "./pages/Contact.js";
+import Pagenotfound from "./pages/Pagenotfound.js";
+import Policy from "./pages/Policy.js";
 function App() {
   return (
-    < >
-      <h1>hello react</h1>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/*" element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
---
+++ client/src/components/layout/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Layout({ children }) {
+  return (
+    <div>
+      <Header />
+      <main style={{ minHeight: "70vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
